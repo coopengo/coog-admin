1f90867a3fb968733309685007be541d3136db25
Pierre Jackman <pierre.jackman@coopengo.com>
2017-03-20 16:30:21 +0100

    update daily_chains

--- cron/daily_chains.sh
+++ cron/daily_chains.sh
@@ -12,9 +12,9 @@ export USER=$USER
 cd $COOG_ADMIN
 
 ./coog chain account_payment_sepa_cog payment --treatment_date=$(date --iso) --payment_kind='payable' --journal_methods='sepa'
-./coog chain account_aggregate snapshot --treatment_date=$(date --iso)
+./coog chain account_aggregate snapshot
 ./coog chain roederer extract_snapshot --treatment_date=$(date --iso) --flush_size='1024' --env_name='ROEDR' --output_filename="/coog/io/batch/extractions/snapshots/snapshots_$(date --iso).csv";
 ./coog chain report_engine produce_request
-#./coog batch ftp.move --input="/coog/io/batch/extractions/" --output="/path/to/ftp/dir"
+#./coog batch ftp.move --input="/usr/local/coog/coog/batch/extractions/snapshots/" --output="/path/to/ftp/dir"
 
 cd -
