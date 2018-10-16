2418cc14932e94ea3771d1d63fd738331b2d7ed8
Frederic Langevin <frederic.langevin@coopengo.com>
2018-10-16 17:28:09 +0200

    Add webat docker management in 2.0

---
+++ cron/daily_chains.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+# This script is to be used by crontab to run daily chains
+
+# 0 3 * * * /home/root/coog-admin/cron/daily_chains.sh
+
+USER=coog-adm
+COOG_ADMIN="$HOME/coog-admin"
+TODAY_DAY="$(date +%e)"
+
+export USER=$USER
+
+cd $COOG_ADMIN
+
+./coog chain account_payment_sepa_cog payment --treatment_date=$(date --iso) --payment_kind='payable' --journal_methods='sepa'
+./coog chain account_aggregate snapshot
+./coog chain roederer extract_snapshot --treatment_date=$(date --iso) --flush_size='1024' --env_name='ROEDR' --output_filename="/workspace/io/batch/extractions/snapshots/snapshots_$(date --iso).txt" --email=gcyganek@roederer.fr,jmarcel@roederer.fr
+./coog chain report_engine produce_request
+./coog batch ftp.move --input="/workspace/io/reports/bdoc/" --output="."
+
+# attente de 20 minutes le temps d'envoyer la bande à la banque et de recevoir l'accusé reception
+echo "##################################################"
+echo "#             Attente de 20 minutes               "
+echo "##################################################"
+
+sleep 1200
+
+echo "##################################################"
+echo "#             Fin attente 20 minutes              "
+echo "##################################################"
+
+./coog chain account_payment_cog payment_ack --treatment_date=$(date --iso) --journal_methods=sepa --payment_kind='payable'
+
+# lancement de la chaine de génération des rente le premier jour du mois
+if [ $TODAY_DAY = "1" ]
+then
+	./coog chain claim_indemnification create_indemnifications --treatment_date=$(date --iso)
+else
+	echo "###################################################"
+	echo "#Pas de lancement du batch de génération des rentes"
+	echo "###################################################"
+fi
+
+cd -
---
+++ cron/migration_cron.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+# This script is to be used by crontab to run migration scripts
+
+# 30 21 * * * /home/root/coog-admin/cron/migration_cron.sh
+
+USER=coog-adm
+COOG_ADMIN="$HOME/coog-admin"
+DOCKER_MIGRATION_DIR="/workspace/io/as400"
+MIGRATION_DIR="$HOME/.local/share/coog/coog/as400"
+BACKUP="backup_$(date "+%d-%m-%y").tar.gz"
+JOB_SIZE=100
+UPDATE=true
+
+export USER=$USER
+
+cd $MIGRATION_DIR/'done'
+
+if [ "$(ls -A *.CSV)" ]; then
+    find . -name "*.CSV" -type f -print0 |
+        tar -czvf ./$BACKUP --null -T -;
+    rm -rf *.CSV
+fi
+
+cd $COOG_ADMIN
+
+./coog redis celery qremove migrator.party
+./coog redis celery qremove migrator.company
+./coog redis celery qremove migrator.group
+./coog redis celery qremove migrator.address
+./coog redis celery qremove migrator.bank
+./coog redis celery qremove migrator.contact
+./coog redis celery qremove migrator.contract
+./coog redis celery qremove migrator.adhesions
+
+./coog batch roederer.file.import dir=$DOCKER_MIGRATION_DIR --job_size=1
+./coog chain roederer_interface migrate-update --job_size=$JOB_SIZE --update=$UPDATE
+
